085ac6b81a363f7ab2b00b4abf3d72a5503f7bf0
safayet <safayetdib@gmail.com>
2023-08-05 16:15:56 +0600

    updated responsive navbar with dark mode feature

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -27,3 +27,34 @@ if (mode !== null) {
     document.documentElement.removeAttribute("data-dark-mode");
   }
 }
+
+const modeSmall = document.getElementById("mode-small-device");
+
+if (modeSmall !== null) {
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      if (event.matches) {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-dark-mode", "");
+      } else {
+        localStorage.setItem("theme", "light");
+        document.documentElement.removeAttribute("data-dark-mode");
+      }
+    });
+
+  modeSmall.addEventListener("click", () => {
+    console.log("clicked small");
+    document.documentElement.toggleAttribute("data-dark-mode");
+    localStorage.setItem(
+      "theme",
+      document.documentElement.hasAttribute("data-dark-mode") ? "dark" : "light"
+    );
+  });
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.documentElement.setAttribute("data-dark-mode", "");
+  } else {
+    document.documentElement.removeAttribute("data-dark-mode");
+  }
+}
